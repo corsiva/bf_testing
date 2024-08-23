8b3b9bef8445a410b21b4daaafd25d7adbe8b94a
corsiva <47104865+corsiva@users.noreply.github.com>
2024-08-23 17:30:49 +0800

    Update EchoBot.cs

--- EchoBot1/EchoBot1/Bots/EchoBot.cs
+++ EchoBot1/EchoBot1/Bots/EchoBot.cs
@@ -12,8 +12,23 @@ namespace EchoBot1.Bots
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            var replyText = $"Echo: {turnContext.Activity.Text}";
-            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            //var replyText = $"Echo: {turnContext.Activity.Text}";
+            //await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            if (string.Equals(turnContext.Activity.Text, "wait", System.StringComparison.InvariantCultureIgnoreCase))
+            {
+                await turnContext.SendActivitiesAsync(
+                    new Activity[] {
+                new Activity { Type = ActivityTypes.Typing },
+                new Activity { Type = "delay", Value= 3000 },
+                MessageFactory.Text("Finished typing", "Finished typing"),
+                    },
+                    cancellationToken);
+            }
+            else
+            {
+                var replyText1 = $"Echo: {turnContext.Activity.Text}. Say 'wait' to watch me type.";
+                await turnContext.SendActivityAsync(MessageFactory.Text(replyText1, replyText1), cancellationToken);
+            }
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
@@ -24,6 +39,7 @@ namespace EchoBot1.Bots
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {
                     await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                    await turnContext.SendActivityAsync(MessageFactory.Text("Test1", "Test2"), cancellationToken);
                 }
             }
         }
